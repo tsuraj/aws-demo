a54d4cbdab2fda648a724fc33ec5f2654db29961
tsuraj <stsurajdeep490@gmail.com>
2025-09-29 22:10:07 +0530

    Initial rails-demo app

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# Gemfile (excerpt)
+source 'https://rubygems.org'
+ruby '3.2.2'
+
+gem 'rails', '~> 7.1'
+gem 'pg', '~> 1.4'
+gem 'puma', '~> 6.0'
+gem 'bootsnap', '>= 1.14.0', require: false
+
+group :development, :test do
+  gem 'pry'
+end
---
+++ config/puma.rb
@@ -0,0 +1,13 @@
+workers Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
+threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS") { 5 })
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "production" }
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
